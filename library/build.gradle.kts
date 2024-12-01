fe9a956b1422b34b4d9c5478c51e7acd006c5ba9
Shangeeth Sivan <shangeeth@poshmark.com>
2024-12-01 21:51:42 +0530

    init

---
+++ library/build.gradle.kts
@@ -0,0 +1,82 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "io.github.kotlin"
+version = "1.0.0"
+
+kotlin {
+    jvm()
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "io.github.shangeethsivan.kmplibrarysample"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "library", version.toString())
+
+    pom {
+        name = "KMP Sample Library"
+        description = "A sample KMP Library that has network layer and repo layer"
+        inceptionYear = "2024"
+        url = "https://github.com/shangeethsivan/kmplibrarysample"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "shangeethsivan"
+                name = "Shangeeth Sivan"
+                url = "https://github.com/shangeethsivan/"
+            }
+        }
+        scm {
+            url = "https://github.com/shangeethsivan/kmplibrarysample/"
+            connection = "scm:git:git://github.com/shangeethsivan/kmplibrarysample.git"
+            developerConnection = "scm:git:ssh://git@github.com/shangeethsivan/kmplibrarysample.git"
+        }
+    }
+}
